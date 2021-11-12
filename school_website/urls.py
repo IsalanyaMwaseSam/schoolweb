31dc6a1d041d382665354a57e4acf71533a4fba9
IsalanyaMwaseSam <eazikezi1999@gmail.com>
2021-11-12 16:47:59 +0300

    first commit

---
+++ school_website/urls.py
@@ -0,0 +1,25 @@
+"""school_website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('education.urls')),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
