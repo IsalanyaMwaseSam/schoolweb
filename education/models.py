5cf286f9f966cae58e8f52821e439a4208bbebe5
IsalanyaMwaseSam <eazikezi1999@gmail.com>
2021-11-30 13:38:41 +0300

    wsgi

--- education/models.py
+++ education/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from education.choices import *
 
+class Courses(models.Model):
+  course = models.CharField(max_length=3, null=True, blank=True, )
+
+  def __str__(self):
+    return self.course
+
+        
+    
+
+
 class Application(models.Model):
   first_name = models.CharField(null=True, max_length=40)
   other_names = models.CharField(null=True, max_length=50)
   campus = models.CharField(max_length=30, blank=True, null=True, choices=CAMPUS_CHOICES)
   study = models.CharField(max_length=20, choices=STUDYTIME_CHOICES, blank=True, null=True,)
   level = models.CharField(max_length=20, choices=LEVEL_CHOICES, blank=True, null=True,)
+  course = models.ForeignKey(Courses, blank=True, null=True, on_delete=models.CASCADE)
   gender = models.CharField(max_length=2, choices=GENDER_CHOICES, blank=True, null=True,)
   courses = models.BooleanField(blank=True, null=True,)
   Date_of_birth = models.CharField(null=True, max_length=15)
