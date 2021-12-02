69b9ddd0a84ec83ccbeeca78d6c24ed909c28231
IsalanyaMwaseSam <eazikezi1999@gmail.com>
2021-12-02 12:05:15 +0300

    setting

--- school_website/settings.py
+++ school_website/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
