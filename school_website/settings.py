de639eed9b5419a63ae61d5f7718043aaed28d42
IsalanyaMwaseSam <eazikezi1999@gmail.com>
2021-12-02 12:16:52 +0300

    static

--- school_website/settings.py
+++ school_website/settings.py
@@ -124,8 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -147,9 +151,7 @@ EMAIL_HOST_PASSWORD = "tcudalqgynwnkynm"
 
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
