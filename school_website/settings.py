7bde03f2d111b99b34b251019b5ddaadb0dcd88a
IsalanyaMwaseSam <eazikezi1999@gmail.com>
2021-12-05 21:29:30 +0300

    heroku

--- school_website/settings.py
+++ school_website/settings.py
@@ -153,11 +153,11 @@ EMAIL_HOST_PASSWORD = "tcudalqgynwnkynm"
 
 
 
-#MIDDLEWARE_CLASSES = (
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-   # 'whitenoise.middleware.WhiteNoiseMiddleware',
-#)
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE_CLASSES = (
+    #Simplified static file serving.
+    #https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #activate django-heroku
 django_heroku.settings(locals())
